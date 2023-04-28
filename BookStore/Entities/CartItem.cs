5e5f176982637df078359929d2798cec9eac6584
saad ablat <saad.ablat@gmail.com>
2023-04-28 19:12:26 +0100

    Initial Push

--- BookStore/Entities/CartItem.cs
+++ BookStore/Entities/CartItem.cs
@@ -6,7 +6,7 @@
         public int CartId { get; set; }
         public Cart Cart { get; set; }
         public int BookId { get; set; }
-        public Book Book { get; set; }
+        public Book Book { get; set; } 
         public int Quantity { get; set; }
     }
 }
